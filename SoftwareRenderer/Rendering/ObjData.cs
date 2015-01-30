8d328c3a8ba8ad4075db7c0847da78fdbebd38c1
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2015-01-31 00:36:47 +0100

    Pawn rendering. Problems with y-axis and scanline count=0

---
+++ SoftwareRenderer/Rendering/ObjData.cs
@@ -0,0 +1,98 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using SoftwareRenderer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareRenderer.Rendering
+{
+    public class IndexedTriangle
+    {
+        public int[] Vertices { get; private set; }
+        public int[] Normals { get; private set; }
+
+        public IndexedTriangle()
+        {
+            Vertices = new int[3];
+            Normals = new int[3];
+        }
+    }
+
+    public class ObjData
+    {
+        public List<Vector<double>> Vertices { get; set; }
+        public List<Vector<double>> Normals { get; set; }
+        public List<IndexedTriangle> Triangles { get; set; }
+
+        public ObjData()
+        {
+            Vertices = new List<Vector<double>>();
+            Normals = new List<Vector<double>>();
+            Triangles = new List<IndexedTriangle>();
+        }
+
+        public static ObjData LoadFromFile(string filePath)
+        {
+            using (var streamReader = new StreamReader(filePath))
+            {
+                var obj = new ObjData();
+
+                string line = null;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (String.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    if (line[0] == '#')
+                    {
+                        continue;
+                    }
+
+                    var tokens = line.Split(' ');
+
+                    switch (tokens[0])
+                    {
+                        case "v":
+                        {
+                            double x = Double.Parse(tokens[1], CultureInfo.InvariantCulture);
+                            double y = Double.Parse(tokens[2], CultureInfo.InvariantCulture);
+                            double z = Double.Parse(tokens[3], CultureInfo.InvariantCulture);
+
+                            obj.Vertices.Add(VectorHelpers.Create(x, -y, z));
+                            break;
+                        }
+                        case "vn":
+                        {
+                            double x = Double.Parse(tokens[1], CultureInfo.InvariantCulture);
+                            double y = Double.Parse(tokens[2], CultureInfo.InvariantCulture);
+                            double z = Double.Parse(tokens[3], CultureInfo.InvariantCulture);
+
+                            obj.Normals.Add(VectorHelpers.Create(x, y, z));
+                            break;
+                        }
+                        case "f":
+                        {
+                            var triangle = new IndexedTriangle();
+                            for (int i = 0; i < 3; ++i)
+                            {
+                                var vertexTokens = tokens[i + 1].Split('/');
+                                triangle.Vertices[i] = Int32.Parse(vertexTokens[0])-1;
+                                triangle.Normals[i] = Int32.Parse(vertexTokens[2])-1;
+                            }
+                            obj.Triangles.Add(triangle);
+                            break;
+                        }
+                    }
+                }
+
+                return obj;
+            }
+        }
+    }
+}
--- SoftwareRenderer/Rendering/Renderer.cs
+++ SoftwareRenderer/Rendering/Renderer.cs
@@ -11,33 +11,37 @@ namespace SoftwareRenderer.Rendering
 {
     public class Renderer : IUpdateable
     {
-        private struct FramebufferData
-        {
-            public double Depth;
-            public int ColorARGB;
-            public double NormalX;
-            public double NormalY;
-            public double NormalZ;
-        }
+        //private struct FramebufferData
+        //{
+        //    public double Depth;
+        //    public int ColorARGB;
+        //    public double NormalX;
+        //    public double NormalY;
+        //    public double NormalZ;
+        //}
 
-        private FramebufferData[,] _framebufferData;
+        //private FramebufferData[,] _framebufferData;
 
         private readonly IRenderWindow _renderWindow;
 
-        private readonly WireframeMesh _cubeWireframe;
+        //private readonly WireframeMesh _cubeWireframe;
         private readonly Mesh _cube;
 
         private readonly Camera _camera;
 
         private double _angle;
 
+        private double[,] _zBuffer;
+
         public TriangleDirection VisibleTriangleDirection { get; set; }
+
+        public ObjData _objData;
 
         public void InitializeBuffers()
         {
             var width = _renderWindow.Framebuffer.PixelWidth;
             var height = _renderWindow.Framebuffer.PixelHeight;
-            _framebufferData = new FramebufferData[width, height];
+            //_framebufferData = new FramebufferData[width, height];
         }
 
         public void ClearBuffers()
@@ -49,18 +53,20 @@ namespace SoftwareRenderer.Rendering
             _renderWindow = renderWindow;
 
             _camera = new Camera();
-            _cubeWireframe = MeshHelpers.GetCubeWireframe(1.0);
+            //_cubeWireframe = MeshHelpers.GetCubeWireframe(1.0);
             _cube = MeshHelpers.GetCube(1.0);
 
             _camera.NearPlane = 0.1;
             _camera.FarPlane = 200.0;
-            _camera.Position = VectorHelpers.Create(0, 0, -5);
+            _camera.Position = VectorHelpers.Create(0, 5, -10);
             _camera.LookAt = VectorHelpers.Create(0, 0, 0);
             _camera.UpVector = VectorHelpers.Create(0, 1, 0);
 
-            _clipper = new CohenSutherlandClipper();
+            //_clipper = new CohenSutherlandClipper();
 
             VisibleTriangleDirection = TriangleDirection.Clockwise;
+
+            _objData = ObjData.LoadFromFile("Data/Models/pawn.obj");
         }
 
         public void Update(TimeSpan elapsedTime)
@@ -68,24 +74,24 @@ namespace SoftwareRenderer.Rendering
             _angle += elapsedTime.TotalSeconds;
         }
 
-        private CohenSutherlandClipper _clipper;
+        //private CohenSutherlandClipper _clipper;
 
-        public void DrawLine3D(WriteableBitmap rt, Matrix<double> transformation,
-            Vector<double> from, Vector<double> to, double hw, double hh)
-        {
-            from = (transformation * from.ExtendVector()).ToCartesian();
-            to = (transformation * to.ExtendVector()).ToCartesian();
+        //public void DrawLine3D(WriteableBitmap rt, Matrix<double> transformation,
+        //    Vector<double> from, Vector<double> to, double hw, double hh)
+        //{
+        //    from = (transformation * from.ExtendVector()).ToCartesian();
+        //    to = (transformation * to.ExtendVector()).ToCartesian();
 
-            if (_clipper.Clip(from, to, out from, out to))
-            {
-                var fromx = (int) ((from[0] + 1)*hw);
-                var fromy = (int) ((from[1] + 1)*hh);
-                var tox = (int) ((to[0] + 1)*hw);
-                var toy = (int) ((to[1] + 1)*hh);
+        //    if (_clipper.Clip(from, to, out from, out to))
+        //    {
+        //        var fromx = (int) ((from[0] + 1)*hw);
+        //        var fromy = (int) ((from[1] + 1)*hh);
+        //        var tox = (int) ((to[0] + 1)*hw);
+        //        var toy = (int) ((to[1] + 1)*hh);
 
-                rt.DrawLine(fromx, fromy, tox, toy, Colors.White);
-            }
-        }
+        //        rt.DrawLine(fromx, fromy, tox, toy, Colors.White);
+        //    }
+        //}
 
         public void RenderFrame()
         {
@@ -102,16 +108,16 @@ namespace SoftwareRenderer.Rendering
             var hw = rt.PixelWidth*0.5;
             var hh = rt.PixelHeight*0.5;
 
-            var zbuffer = new double[rt.PixelWidth, rt.PixelHeight];
+            _zBuffer = new double[rt.PixelWidth, rt.PixelHeight];
             for (int y = 0; y < rt.PixelHeight; ++y)
             {
                 for (int x = 0; x < rt.PixelWidth; ++x)
                 {
-                    zbuffer[x, y] = Double.MaxValue;
+                    _zBuffer[x, y] = Double.MaxValue;
                 }
             }
 
-            for (var i = 0; i + 2 < _cube.Vertices.Count; i += 3)
+            /*for (var i = 0; i + 2 < _cube.Vertices.Count; i += 3)
             {
                 var a = (projView * _cube.Vertices[i + 0].Position.ExtendVector()).ToCartesian()
                     .Add(1.0).PointwiseMultiply(VectorHelpers.Create(hw, hh, 1.0));
@@ -121,7 +127,9 @@ namespace SoftwareRenderer.Rendering
                     .Add(1.0).PointwiseMultiply(VectorHelpers.Create(hw, hh, 1.0)); ;
 
                 DrawScreenSpaceTriangle(rt, zbuffer, new Vector<double>[] { a, b, c });
-            }
+            }*/
+
+            RenderObjMesh(_objData, projView);
 
             rt.Unlock();
         }
@@ -144,6 +152,26 @@ namespace SoftwareRenderer.Rendering
             DrawScreenSpaceTriangleInterpolated(rt, zbuffer, triangle[0][0], triangle[0][1], triangle[0][2],
                 triangle[1][0], triangle[1][1], triangle[1][2], triangle[2][0], triangle[2][1],
                 triangle[2][2], Colors.Red, Colors.Blue, Colors.Green);
+        }
+
+        public void RenderObjMesh(ObjData meshToRender, Matrix<double> transformation)
+        {
+            var hw = _renderWindow.Framebuffer.PixelWidth * 0.5;
+            var hh = _renderWindow.Framebuffer.PixelHeight * 0.5;
+
+            var transformedMeshVertices = meshToRender.Vertices
+                .Select(t => (transformation*t.ExtendVector()).ToCartesian().Add(1.0).PointwiseMultiply(VectorHelpers.Create(hw, hh, 1.0)))
+                .ToList();
+
+            var transformedMeshNormals = meshToRender.Normals.ToList();
+
+            foreach (var triangle in meshToRender.Triangles)
+            {
+                DrawScreenSpaceTriangle(_renderWindow.Framebuffer, _zBuffer, new[] { 
+                    transformedMeshVertices[triangle.Vertices[0]],
+                    transformedMeshVertices[triangle.Vertices[1]],
+                    transformedMeshVertices[triangle.Vertices[2]] });
+            }
         }
 
         public enum TriangleDirection
@@ -228,11 +256,16 @@ namespace SoftwareRenderer.Rendering
                 scanEdges.Add(scanEdge);
             }
 
+            if (scanEdges.Count == 0)
+            {
+                return;
+            }
+
             scanEdges = scanEdges.OrderBy(t => t.MinimalY).ToList();
             int scanEdgesActivated = 0;
 
-            var startY = scanEdges.Min(t => t.MinimalY);
-            var endY = scanEdges.Max(t => t.MaximalY);
+            var startY = Math.Max(scanEdges.Min(t => t.MinimalY), 0);
+            var endY = Math.Min(scanEdges.Max(t => t.MaximalY), rt.PixelHeight-1);
 
             var activeEdges = new List<ScanEdge>();
             for (var y = startY; y <= endY; ++y)
@@ -254,8 +287,8 @@ namespace SoftwareRenderer.Rendering
 
                 for (int i = 0; i < activeEdges.Count - 1; i += 2)
                 {
-                    int startX = (int)activeEdges[i].X;
-                    int endX = (int)activeEdges[i + 1].X;
+                    int startX = Math.Max((int)activeEdges[i].X, 0);
+                    int endX = Math.Min((int)activeEdges[i + 1].X, rt.PixelWidth-1);
 
                     for (int x = startX; x <= endX; ++x)
                     {
